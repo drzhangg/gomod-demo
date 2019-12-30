70226b2ec7819f2285fd2872b3bdb33e9b27c9f1
drzhang <654014730@qq.com>
2019-12-30 10:08:51 +0800

    learn go

--- test/demo3.go
+++ test/demo3.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	$END$
+	year := time.Now().Format("2006")
+	month := time.Now().Format("01")
+	day := time.Now().Format("02")
+
+	fmt.Println(year+"-"+month+"-"+day)
 }
