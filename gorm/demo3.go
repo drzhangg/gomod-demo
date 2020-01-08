a6bd5480dccf73c2ad032bc340d63026ca0060a8
drzhang <654014730@qq.com>
2020-01-08 15:00:24 +0800

    learn go

--- gorm/demo3.go
+++ gorm/demo3.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	
+	now := time.Now()
+	currentYear, currentMonth, _ := now.Date()
+	currentLocation := now.Location()
+	firstOfMonth1 := time.Date(currentYear, currentMonth, 1, 0, 0, 0, 0, currentLocation)
+	lastOfMonth := firstOfMonth1.AddDate(0, 1, -1).Format("2006-01-02")
+	firstOfMonth:=firstOfMonth1.Format("2006-01-02")
+
+	fmt.Println(lastOfMonth)
+	fmt.Println(firstOfMonth)
 }
