2bb821c3e555df0c9cdc0d19d5cc1f046e062bd6
drzhang <654014730@qq.com>
2020-01-16 19:44:30 +0800

    learn go

--- test/demo5.go
+++ test/demo5.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("hello")
+	ch := make(chan int)
+	go put(ch)
+	go get(ch)
 }
+
+func put(ch chan int) {
+	ch <- 1
+}
+
+func get(ch chan int)  {
+	<- ch
+}
+
