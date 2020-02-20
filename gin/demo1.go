8a5238eb0a4210fe1d7b719e77ed6ac2c6f490d1
drzhang <654014730@qq.com>
2020-02-20 22:04:53 +0800

    logAgent

--- gin/demo1.go
+++ gin/demo1.go
@@ -2,29 +2,37 @@ package main
 
 import (
 	"fmt"
-	"gomod-demo/demo"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
+type Login struct {
+	User string `json:"user"`
+	Pwd  string `json:"pwd"`
+}
+
 func main() {
-	demo.Service()
+	r := gin.Default()
 
-	v1 := make([]int,0)
-	fmt.Println(len(v1))
-	fmt.Println(cap(v1))
+	r.POST("/login", func(c *gin.Context) {
+		//var l Login
+		//if err := c.ShouldBind(&l); err != nil {
+		//	fmt.Println("shouldbind failed,err:", err)
+		//	return
+		//}
+		user,_ := c.Get("user")
 
-	fmt.Println("---------")
+		fmt.Println("user:",c.Param("user"))
+		fmt.Println("user2:",user.(string))
+		fmt.Println("pwd:",c.Param("pwd"))
 
-	v1 = append(v1, 1)
-	fmt.Println(len(v1))
-	fmt.Println(cap(v1))
-	fmt.Println("---------")
+		if c.Param("user") == "zhang" && c.Param("pwd") == "123"  {
+			c.JSON(http.StatusOK,gin.H{"msg":"login success"})
+		}else {
+			c.JSON(http.StatusOK,gin.H{"err":"user or pwd failed"})
+		}
 
-	v1 = append(v1, 2)
-	fmt.Println(len(v1))
-	fmt.Println(cap(v1))
-	fmt.Println("---------")
-	v1 = append(v1, 3)
-	fmt.Println(len(v1))
-	fmt.Println(cap(v1))
+	})
 
+	r.Run(":8080")
 }
