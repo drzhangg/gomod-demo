0cbde25d3495e3d1cfa39ae77946421a608dca47
drzhang <654014730@qq.com>
2020-02-23 00:36:27 +0800

    learn go

--- gin/demo3.go
+++ gin/demo3.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -10,12 +11,37 @@ func main() {
 	router := gin.Default()
 
 	router.POST("/form", func(c *gin.Context) {
-		type1 := c.DefaultPostForm("type","alert")
+		//	type1 := c.DefaultPostForm("type","alert")
+		//
+		//	username := c.PostForm("username")
+		//	password := c.PostForm("password")
+		//	hobbys := c.PostFormArray("hobby")
+		//	c.String(http.StatusOK, fmt.Sprintf("type is %v, username is %v, password is %v, hobby is %v", type1, username, password, hobbys))
 
-		username := c.PostForm("username")
-		password := c.PostForm("password")
-		hobbys := c.PostFormArray("hobby")
-		c.String(http.StatusOK, fmt.Sprintf("type is %v, username is %v, password is %v, hobby is %v", type1, username, password, hobbys))
+		//file, _ := c.FormFile("file")
+		//
+		//if err := c.SaveUploadedFile(file, "/Users/drzhang"); err != nil {
+		//	fmt.Sprintf("save failed,err :", err.Error())
+		//}
+
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusBadGateway, fmt.Sprintf("get err %v", err.Error()))
+		}
+		files := form.File["file"]
+		for _, file := range files {
+			bytes, err := ioutil.ReadFile(file.Filename)
+			if err != nil {
+				fmt.Println("read file failed, err:", err)
+			}
+			filename := "/Users/drzhang/"
+			err = ioutil.WriteFile(filename, bytes, 777)
+			if err != nil {
+				fmt.Println("write file failed, err:", err)
+			}
+		}
+		c.String(http.StatusOK, fmt.Sprintf("file is %v", len(files)))
+
 	})
 	router.Run(":8080")
 }
