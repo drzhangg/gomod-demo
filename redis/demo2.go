f265993294fd318bbcd99537c98c277d6be99af1
drzhang <654014730@qq.com>
2020-03-03 21:15:36 +0800

    redis hset

--- redis/demo2.go
+++ redis/demo2.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "47.103.9.218:6379",
+		Password: "",
+		DB:       0,
+	})
+	bcmd := client.HSet("htest", "a", 123)
+	fmt.Println(bcmd.Val())
+	bcmd = client.HSet("htest", "b", "ccc")
+	fmt.Println(bcmd.Val())
+
+	scmd := client.HGet("htest", "b")
+	fmt.Println(scmd.String())
+}
