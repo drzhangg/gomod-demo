d5f11253a1479392acde99120cea21c1f0396146
drzhang <654014730@qq.com>
2020-03-17 23:24:37 +0800

    test

--- test/demo13.go
+++ test/demo13.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	p := make([]int,1)
+	p := make([]int,2)
+	//p = append(p, 1)
+
+	p1 := p[2:]
 	//p = append(p, 1)
 	fmt.Println("len:", len(p))
 	fmt.Println("cap:", cap(p))
-	fmt.Println(p[1:])
+	fmt.Println(p1)
 
+	fmt.Println("len:", len(p1))
+	fmt.Println("cap:", cap(p1))
+
+	var s []string
+	s = append(s, "jerry")
 }
