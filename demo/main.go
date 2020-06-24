6cc708e1f11eace4ef3b5dbd358814a4955e76cb
drzhang <654014730@qq.com>
2020-06-24 17:12:01 +0800

    demo

--- demo/main.go
+++ demo/main.go
@@ -2,27 +2,11 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	var name string
-	var age int
-	var  salay float32
-	var ispass string
-
-	fmt.Println("请输入名字")
-	//time.Sleep(20)
-	fmt.Scanln(&name)    //使用指针  改变name的值
-
-	fmt.Println("请输入年龄")
-	fmt.Scanln(&age)    //使用指针  改变age的值
-
-	fmt.Println("请输入薪水")
-	fmt.Scanln(&salay)    //使用指针  改变name的值
-
-	fmt.Println("请输入是否通过考试")
-	fmt.Scanln(&ispass)    //使用指针  改变name的值
-
-	fmt.Println(name,age,salay,ispass)
+	fmt.Println(time.Now())
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 
 }
