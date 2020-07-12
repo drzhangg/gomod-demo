02dcadde6264756bec3740de0725613cbeec3d8d
drzhang <654014730@qq.com>
2020-07-12 20:04:27 +0800

    go-micro get service

--- go-micro/test.go
+++ go-micro/test.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"github.com/micro/go-micro/client/selector"
 	"github.com/micro/go-micro/registry"
+	"github.com/micro/go-plugins/registry/consul"
 	"log"
 )
 
 func main() {
 
-	services, err := registry.GetService("prodservice1")
+	consulReg := consul.NewRegistry(
+		registry.Addrs("47.103.9.218:8500"))
+
+	services, err := consulReg.GetService("prodservice1")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	next := selector.Random(services)
+	node, err := next()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(node.Id, node.Address, node.Metadata)
 }
