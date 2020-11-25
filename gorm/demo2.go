45a77d140861f6687dbfa5566700147f7c11c315
drzhang <654014730@qq.com>
2020-11-25 21:54:08 +0800

    defer panic recover

--- gorm/demo2.go
+++ gorm/demo2.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
@@ -10,6 +11,8 @@ type User struct {
 	Name string `db:"name"`
 	Age  int64  `db:"age"`
 }
+
+var Db *gorm.DB
 
 //root:root@tcp(localhost:3306)/demo
 const (
@@ -21,29 +24,59 @@ const (
 	database = "test1"
 )
 
-func main() {
-	//db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@%s(%s:%d)/%s", username, password, network, server, port, database))
-
-	db, err := gorm.Open("mysql", "root:root@(47.103.9.218:3306)/test1?charset=utf8")
+func init() {
+	db, err := gorm.Open("mysql", "root:root123456@(47.103.9.218:3306)/test?charset=utf8")
 	if err != nil {
 		fmt.Printf("Open mysql failed,err:%v\n", err)
 		return
 	}
-	defer db.Close()
+	//defer Db.Close()
 
 	fmt.Println(db.DB().Ping())
 
 	db.SingularTable(true)
 	db.LogMode(true)
 
-	QueryOne(db)
-	Insert(db)
+	Db = db
+}
+
+func main() {
+
+	r := gin.Default()
+	r.GET("/getUser", GetUser)
+
+	r.Run(":8082")
+}
+
+func GetUser(c *gin.Context) {
+	name := c.Query("name")
+	fmt.Println(name)
+	user, err := GetUserByName(name)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"err": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"data": user,
+	})
+}
+
+func GetUserByName(name string) (user User, err error) {
+	//err = Db.Table("user").Where(fmt.Sprintf("name = '%s'",name)).First(&user).Error
+	err = Db.Table("user").Where("name = ?", name).First(&user).Error
+	return user, err
+}
+
+func GetUserByName2(name string) (user User, err error) {
+	err = Db.Raw("select * from user where name = '" + name + "'").Scan(&user).Error
+	return user, err
 }
 
 //Query
 func QueryOne(db *gorm.DB) {
 	var user User
-	//db.Find(&user)
 	db.Raw("select name,age from user where name = ?", "jerry").Scan(&user)
 
 	fmt.Println(user)
